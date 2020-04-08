7a761e57ea64e8a099d4d013fb736d354d742c09
mrvnre <mrvnre24@gmail.com>
2020-04-08 11:20:50 -0500

    Created new repo and new project

--- angular_practice/my-project-name/src/app/app.component.ts
+++ angular_practice/my-project-name/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-project-name';
+  name: string = 'Barbara Liskov';
+  itemList: string[] = ['tea', 'coffee', 'milk', 'some desert']
+  title: string = 'my-project-name';
+  rectangle = {
+    length: 5,
+    width: 6,
+    area: function() {
+       return this.length * this.width;
+    }
+ }
 }
