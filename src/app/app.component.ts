888c2671de479d6f2845627afe9e23958c3ff270
mrvnre <mrvnre24@gmail.com>
2020-04-13 22:27:12 -0500

    Highlighted space debris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,11 @@ export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
   displayList: Satellite[];
+  
   constructor() {
     this.sourceList = [];
     this.displayList = [];
-    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json'; 
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
@@ -25,13 +26,23 @@ export class AppComponent {
               let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
               this.sourceList.push(satellite);
             }
-             
-          // TODO: loop over satellites
-          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- 
-       }.bind(this));
+            
+          this.displayList = this.sourceList.slice(0);
+      }.bind(this));
     }.bind(this));
-   this.displayList = this.sourceList.slice(0);
+  }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
  }
+  
 } 
